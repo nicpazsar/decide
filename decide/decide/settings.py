4a54f83f449ed37a0b8147529338360bcf304bf0
Nicolás Pazos <nicpazsar@alum.us.es>
2021-02-06 19:52:26 +0100

    Cambios funcionales

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
+        'NAME': 'decidedb',
         'USER': 'decide',
         'PASSWORD': 'decide',
         'HOST': 'localhost',
